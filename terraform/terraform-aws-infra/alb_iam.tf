a1b58d3ea16e74d8faa4983738cdf275cc7e1a13
Max-Medov <max19900@gmail.com>
2024-12-31 21:41:59 +0200

    iam

--- terraform/terraform-aws-infra/alb_iam.tf
+++ terraform/terraform-aws-infra/alb_iam.tf
@@ -1,8 +1,9 @@
 locals {
   splitted_oidc = split("oidc-provider/", module.eks.oidc_provider_arn)
-  oidc_url = local.splitted_oidc[1]
+  oidc_url = local.splitted_oidc[1] # e.g. "oidc.eks.us-east-1.amazonaws.com/id/<OIDC_ID>"
 }
 
+# 2) Create the trust policy so the ALB Controller Pod can assume this role
 data "aws_iam_policy_document" "alb_controller_assume_role" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -12,18 +13,18 @@ data "aws_iam_policy_document" "alb_controller_assume_role" {
     }
     condition {
       test     = "StringEquals"
-      # This yields something like:
-      #   "oidc.eks.us-east-1.amazonaws.com/id/<UNIQUE_ID>:sub"
+      # e.g. "oidc.eks.us-east-1.amazonaws.com/id/<OIDC_ID>:sub"
       variable = "${local.oidc_url}:sub"
 
       values = [
-        # Must match your serviceAccount's namespace/name
+        # Must match your ServiceAccount's "namespace:name"
         "system:serviceaccount:kube-system:aws-load-balancer-controller"
       ]
     }
   }
 }
 
+# 3) Create the IAM Role with the above trust policy
 resource "aws_iam_role" "alb_controller" {
   name               = "alb-controller-irsa-role"
   assume_role_policy = data.aws_iam_policy_document.alb_controller_assume_role.json
@@ -34,8 +35,42 @@ resource "aws_iam_role" "alb_controller" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "alb_controller_recommended" {
-  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
+data "aws_iam_policy_document" "alb_controller_official_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:DescribeAccountAttributes",
+      "ec2:DescribeAddresses",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeInternetGateways",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeVpcs",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "elasticloadbalancing:*",
+    ]
+    resources = ["*"]
+  }
+
+}
+
+# 5) Create a policy from the above JSON
+resource "aws_iam_policy" "alb_controller_custom_policy" {
+  name   = "alb-controller-custom-policy"
+  policy = data.aws_iam_policy_document.alb_controller_official_policy.json
+}
+
+# 6) Attach the custom policy to our role
+resource "aws_iam_role_policy_attachment" "alb_controller_custom_attach" {
+  policy_arn = aws_iam_policy.alb_controller_custom_policy.arn
   role       = aws_iam_role.alb_controller.name
 }
+
+
 
